6463236e91c407398829794a89299aed2a6bce8a
Unknown <egesaygili@hotmail.com>
2018-07-06 21:30:03 +0300

    Example Course Created

--- ITUScheduleManager/ITUScheduleManager/Form1.cs
+++ ITUScheduleManager/ITUScheduleManager/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ITUScheduleManager.CurriculumLibrary;
+using ITUScheduleManager.CurriculumLibrary.SemesterLibrary.CourseLibrary;
 
 namespace ITUScheduleManager
 {
@@ -15,6 +17,24 @@ namespace ITUScheduleManager
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Course Ege1 = new Course();
+            Ege1.Branch = "MAT";
+            Ege1.Building = "MED";
+            Ege1.Capacity = 50;
+            Ege1.Code = "MAT 103";
+            Ege1.Credits = 4;
+            Ege1.Crn = "30411";
+            Ege1.Departments = "BIO, CEV, CHZ, DEN, END, FIZ, GEM, GEMM, GEO, GID, IML, ISL, JDF, JEF, JEO, KIM, KIMM, KMM, KMMM, MAD, MAK, MAKM, MATM, MET, MTO, PET, PETM, TEK, UCK, UCKM, UZB, UZBM".Trim().Split(',');
+            Ege1.Enrolled = 47;
+            Ege1.Grade = Grading.BB;
+            Ege1.Instructor = "Mehmet Ali Karaca";
+            Ege1.Room = "A12";
+            Ege1.Title = "Matematik I";
         }
     }
 }
